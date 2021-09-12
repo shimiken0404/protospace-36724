49838b04eaf44f9cd5b73a1ffbc25b8447b5110a
shimiken0404 <shimizu.kenta0404@gmail.com>
2021-09-12 23:09:08 +0900

    ユーザー管理

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,25 @@
 class PrototypesController < ApplicationController
-  def index
+
+  def _form
   end
+
+  def _prototype
+  end
+
+  def edit
+  end
+
+  def index
+
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+ 
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-
+  devise_for :users #デバイスをしようするためのもの
+  
   root to: "prototypes#index"
- # resources :
+  #resources :users
 end
